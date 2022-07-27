45e38075ac6a02548442927cca62ba5c2c0c21b0
manyblake <manyblake@gmail.com>
2022-07-27 14:25:40 +0200

    ciclo while

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 const listaSpesa = [`pane`, `pasta`, `farina`, `sale`, `zucchero`];
 const list = document.querySelector(`.lista-spesa`);
+let i = 0;
 
-for (let i = 0; i < listaSpesa.length; i++) {
+for (i = 0; i < listaSpesa.length; i++) {
   const li = document.createElement("li");
   li.innerHTML = listaSpesa[i];
   list.appendChild(li);
+}
+
+i = 0;
+while (i < listaSpesa.length) {
+  const li = document.createElement("li");
+  li.innerHTML = listaSpesa[i];
+  list.appendChild(li);
+  i++;
 }
